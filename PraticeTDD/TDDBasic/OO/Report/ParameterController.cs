5b4449f0fe74eda4aea5d85dad5cd50c86cf0b3e
agiledon <zhangyi1101@tom.com>
2019-09-02 14:58:28 +0800

    add example of template pattern

--- PraticeTDD/TDDBasic/OO/Report/ParameterController.cs
+++ PraticeTDD/TDDBasic/OO/Report/ParameterController.cs
@@ -10,39 +10,46 @@ namespace Zhangyi.PraticeTDD.TDDBasic.OO.Report
         {
             foreach (IParameter para in parameterGraph.BuildParameters())
             {
-                if (para is SimpleParameter) {
+                if (para is SimpleParameter)
+                {
                     SimpleParameter simplePara = para as SimpleParameter;
                     string[] values = request.ParameterValues(simplePara.Name);
-                simplePara.Values = values;
-            } else {
-                if (para is ItemParameter) {
-                    ItemParameter itemPara = para as ItemParameter;
-                    foreach (Item item in itemPara.Items)
+                    simplePara.Values = values;
+                }
+                else
+                {
+                    if (para is ItemParameter)
                     {
-                        string[] values = request.ParameterValues(item.Name);
-                        item.Values = values;
+                        ItemParameter itemPara = para as ItemParameter;
+                        foreach (Item item in itemPara.Items)
+                        {
+                            string[] values = request.ParameterValues(item.Name);
+                            item.Values = values;
+                        }
                     }
-                } else {
-                    TableParameter tablePara = para as TableParameter;
-                    string[] rows =
+                    else
+                    {
+                        TableParameter tablePara = para as TableParameter;
+                        string[] rows =
                             request.ParameterValues(tablePara.RowName);
                         string[] columns =
                             request.ParameterValues(tablePara.ColumnName);
                         string[] dataCells =
                             request.ParameterValues(tablePara.DataCellName);
 
-                    int columnSize = columns.Length;
-                    for (int i = 0; i < rows.Length; i++)
-                    {
-                        for (int j = 0; j < columns.Length; j++)
+                        int columnSize = columns.Length;
+                        for (int i = 0; i < rows.Length; i++)
                         {
-                            TableParameterElement element = new TableParameterElement(rows[i], columns[j], dataCells[columnSize * i + j]);
-                            tablePara.AddElement(element);
+                            for (int j = 0; j < columns.Length; j++)
+                            {
+                                TableParameterElement element = new TableParameterElement(rows[i], columns[j],
+                                    dataCells[columnSize * i + j]);
+                                tablePara.AddElement(element);
+                            }
                         }
                     }
                 }
             }
         }
     }
-}
 }
---
+++ PraticeTDD/TDDBasic/OO/Template/PartDB.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Zhangyi.PracticeTDD.TDDBasic.OO.Template
+{
+    public class PartDb
+    {
+        private IList<Part> parts = new List<Part>();
+
+        public void Populate()
+        {
+            const string connString = "Data Source=(local);Initial Catalog=Northwind;"
+                                      + "Integrated Security=true";
+            const string queryString = "select * from part";
+
+
+            using (var connection = new SqlConnection(connString))
+            {
+                var command = new SqlCommand(queryString, connection);
+
+                try
+                {
+                    connection.Open();
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string name = reader[0].ToString();
+                        string brand = reader[1].ToString();
+                        double retailPrice = double.Parse(reader[2].ToString());
+
+                        parts.Add(new Part(name, brand, retailPrice));
+                    }
+
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+
+    class Part
+    {
+        private string name;
+        private string brand;
+        private double retailPrice;
+
+        public Part(string name, string brand, double retailPrice)
+        {
+            this.name = name;
+            this.brand = brand;
+            this.retailPrice = retailPrice;
+        }
+
+        public string Name => name;
+
+        public string Brand => brand;
+
+        public double RetailPrice => retailPrice;
+    }
+}
